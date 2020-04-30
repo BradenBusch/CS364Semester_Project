501888b71b4fdedcd4fdce55dce197bfa029d5d2
Graham-Nelson73 <47195121+Graham-Nelson73@users.noreply.github.com>
2020-04-29 19:05:19 -0500

    Poured a little sauce on it

--- CS364Semester_Project/settings.py
+++ CS364Semester_Project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 	'localflavor',
 	'project.apps.ProjectConfig',
-	'polls.apps.PollsConfig',
+	#'polls.apps.PollsConfig',
 	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
